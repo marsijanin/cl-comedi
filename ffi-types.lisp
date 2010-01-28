dd4be9d2da0be23e59d009868b3d5959282c1937
Nikolay V. Razbegaev <marsijanin@gmail.com>
2010-01-28 12:40:54 +0300

    add comedi_insn wrappers; fix struct comedi_range slot name misprint

--- ffi-types.lisp
+++ ffi-types.lisp
@@ -12,5 +12,28 @@
 (cstruct range "comedi_range"
   (min  "min"  :type :double)
   (max  "max"  :type :double)
-  (init "unit" :type :unsigned-int))
+  (unit "unit" :type :unsigned-int))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(constantenum (instructions-types :define-constants t)
+  ((:read "INSN_READ")
+   :documentation "read values from an input channel")
+  ((:write "INSN_WRITE")
+   :documentation "write values to an output channel")
+  ((:bits "INSN_BITS")
+   :documentation "read/write values on multiple digital I/O channels")
+  ((:config "INSN_CONFIG")
+   :documentation "configure a subdevice")
+  ((:gtod "INSN_GTOD")
+   :documentation "read a timestamp, identical to gettimeofday()")
+  ((:wait "INSN_WAIT")
+   :documentation "wait a specified number of nanoseconds"))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(cstruct instructions "comedi_insn"
+  #|(instructions "insn"      :type :unsigned-int)|#
+  (instructions "insn"      :type instructions-types)
+  (n             "n"        :type :unsigned-int)
+  (data          "data"     :type :pointer)
+  (subdevice     "subdev"   :type :unsigned-int)
+  (channels      "chanspec" :type :unsigned-int)
+  (unused        "unused"   :type :pointer))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
