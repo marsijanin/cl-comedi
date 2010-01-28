316af4c31e4fe2e67f5f5569802e3eae706715f3
Nikolay V. Razbegaev <marsijanin@gmail.com>
2010-01-28 12:12:27 +0300

    update pkgdecl.lisp

--- pkgdcl.lisp
+++ pkgdcl.lisp
@@ -1,7 +1,26 @@
 ;; -*- Mode: Lisp; Syntax: ANSI-Common-Lisp; indent-tabs-mode: nil -*-
 ;; comedilib cffi wrappers: package definition
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defpackage :cl-comedi
-  (:nicknames :comedi)
-  (:use :common-lisp :cffi :iolib.syscalls))
+(in-package :common-lisp-user)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defpackage #:cl-comedi
+  (:nicknames #:comedi)
+  (:use :common-lisp :cffi :iolib.syscalls)
+  (:export 
+   ;; C types and constants:
+   #:lsample-t
+   #:aref-ground
+   #:range
+   ;; C functions wrapers:
+   #:%open
+   #:%close
+   #:%data-read
+   #:%get-maxdata
+   #:%get-range
+   #:%get-n-ranges
+   #:%to-phys
+   #:%from-phys
+   ;; Some more lispish stuff:
+   #:with-comedi-device-pointer
+   #:get-range))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
